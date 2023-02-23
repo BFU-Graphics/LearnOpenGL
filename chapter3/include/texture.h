78a25a860eb8f30b957b7386864c63baaaebad6c
CelesteOvO <1227657366@qq.com>
2023-02-23 16:13:58 +0800

    Chapter3 Initial Submission

---
+++ chapter3/include/texture.h
@@ -0,0 +1,21 @@
+#ifndef LEARNOPENGL_TEXTURE_H
+#define LEARNOPENGL_TEXTURE_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class Texture
+{
+public:
+    unsigned int ID;
+
+public:
+    Texture(const std::string &texture_path);
+    Texture();
+    void bind();
+};
+#endif //LEARNOPENGL_TEXTURE_H
